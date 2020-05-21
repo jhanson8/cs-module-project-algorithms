9b05b4a43c28e5f453410811fca7726d5ab04c3c
Jonathan Hanson <jhanson8@uoregon.edu>
2020-05-21 12:34:04 -0700

    cookies and single number optimized

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,9 +2,31 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
+'''def single_number(arr):
     # Your code here
-    return 2 * sum(set(arr)) - sum(arr) 
+    return 2 * sum(set(arr)) - sum(arr)''' 
+
+
+
+def single_number(nums):
+    # keep track of the counts of how many times we've seen a particular number 
+    # dictionaries are better at being searched 
+    counts = {}
+
+    #O(n)
+    # loop through nums to tally up how many times we've seen each number 
+    for x in nums:
+        if x in counts: #O(1)
+            del counts[x]
+        else:
+            counts[x] = 1
+
+    # loop through all of the key-value pairs in counts to find the one pair
+    # whose value is 1
+    #O(1)
+    for num in counts:
+        if counts[num] == 1:
+            return num
 
 
 
